990b2a380960c61c75860970859f4aec00f47964
Aroslav Kochkin <kochkin.2328@inbox.ru>
2023-01-17 20:43:00 +0700

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Ярослав Кочкин on 17.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
